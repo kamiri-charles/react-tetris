41c715ce4d2ae768460ae9f177cf5d7e1315739f
Charles Kamiri <charleskamiri6@gmail.com>
2023-03-09 13:39:37 +0300

    Routing bug fix.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Canvas from './components/Canvas';
 import './App.scss';
 
 function App() {
 	return (
 		<div className="App">
-			<Canvas />
+
+			<Router basename='react-tetris'>
+				<Routes>	
+					<Route path="/" element={<Canvas />} />
+				</Routes>
+			</Router>
 		
 		</div>
 		);
